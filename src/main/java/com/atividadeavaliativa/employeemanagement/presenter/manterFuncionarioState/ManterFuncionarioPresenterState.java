4aad1f49deef18d9f24e2481a1294ea04c92b307
= <werikis90@hotmail.com>
2021-04-30 20:17:42 -0300

    Altereçao na BuscarFuncionarioState

--- src/main/java/com/atividadeavaliativa/employeemanagement/presenter/manterFuncionarioState/ManterFuncionarioPresenterState.java
+++ src/main/java/com/atividadeavaliativa/employeemanagement/presenter/manterFuncionarioState/ManterFuncionarioPresenterState.java
@@ -42,58 +42,5 @@ public abstract class ManterFuncionarioPresenterState {
             }
         }
     }
-
-
-    /*
-    public void salvar() throws Exception {
-    }
-
-    public void editar() throws Exception {
-    }
-
-    public void excluir() throws Exception {
-    }
-
-    public void cancelar() {
-    }
-
-    public final void fechar() {
-        this.presenter.getView().setVisible(false);
-        this.presenter.getView().dispose();
-    }
-
-    private void limpaListeners() {
-
-        for (Component c : this.presenter.getView().getContentPane().getComponents()) {
-            if (c instanceof JButton) {
-                for (ActionListener al : ((JButton) c).getActionListeners()) {
-                    ((JButton) c).removeActionListener(al);
-                }
-            }
-        }
-    }
-
-    protected Funcionario lerDados() throws Exception {
-        String nome = presenter.getView().getTxtNome().getText();
-        String cargo = presenter.getView().getTxtCargo().getText();
-        double salarioBase = 0;
-        if (!presenter.getView().getTxtSalarioBase().getText().isEmpty()) {
-            salarioBase = Double.parseDouble(presenter.getView().getTxtSalarioBase().getText().replace(",", "."));
-
-        }
-        Funcionario f = new Funcionario(nome, cargo, salarioBase);
-
-        if (!presenter.getView().getTxtFaltas().getText().isEmpty()) {
-            int faltas = Integer.parseInt(presenter.getView().getTxtFaltas().getText());
-            f.setFaltas(faltas);
-        }
-
-        if (!presenter.getView().getTxtDistancia().getText().isEmpty()) {
-            double distancia = Double.parseDouble(presenter.getView().getTxtDistancia().getText());
-            f.setDistanciaDoTrabalho(distancia);
-        }
-        
-        return f;
-    }
-     */
+   
 }
